5609cc9031f0c6f6dd7037657180f901c25c04ec
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-03 15:28:57 +0100

    the current implementation with the bug

---
+++ unFinishedLoopBug.java
@@ -0,0 +1,21 @@
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class unFinishedLoopBug {
+
+    public static List<Integer> CreateList(int number){
+        List<Integer> list = new ArrayList<>(number);
+        for (int count = 1; count <= number;) {
+            list.add(count);
+        }
+        return list;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(CreateList(5));
+        System.out.println("test");
+    }
+}
