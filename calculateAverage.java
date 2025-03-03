fa0be4a6c6cb454152d0b21d8d7c8bd3710b1878
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-03 13:43:04 +0100

    declare variables for the array length and sum

--- calculateAverage.java
+++ calculateAverage.java
@@ -1,6 +1,26 @@
+// Write a function which calculates the average of the numbers in a given array.
+// Note: Empty arrays should return 0.
+
 public class calculateAverage {
-    public static double main(String[] args) {
-     return 0;
+
+    public static double calculateAverageFunc(int[] array) {
+        int arrayLength = array.length;
+        int sum = 0;
+
+      if(array.length == 0){
+        return 0;
+      }
+
+      for(int i = 0; i < arrayLength; i++){
+        sum += array[i];
+      }
+      return sum / arrayLength;
+      
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Testing this code ");
+        
     }
 }   
 
