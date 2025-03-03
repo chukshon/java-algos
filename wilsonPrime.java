ec8eedc37c3ced38ddf0a9da83713743b5444e43
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-03 14:05:38 +0100

    created a isWilsonPrime method to check if the parameter is wilson prime or false

---
+++ wilsonPrime.java
@@ -0,0 +1,21 @@
+// Wilson primes satisfy the following condition. Let 
+// P represent a prime number.
+
+// Then, (P−1)!+1P∗P should give a whole number, where 
+// P! is the factorial of P
+
+// Your task is to create a function that returns true if the given number is a Wilson prime and false otherwise.
+
+
+
+public class wilsonPrime {
+    
+    public static long isWilsonPrime(long number){
+
+        return 0;    
+    }
+
+   public static void main(String[] args) {
+    System.out.println("test");
+   }
+}
