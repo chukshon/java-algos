4224826ec7e74ca8d22af320149846d5989238bc
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-05 13:52:48 +0100

    converted the input string to upper case

---
+++ makeUpperCase.java
@@ -0,0 +1,10 @@
+// Description: Write a function which converts the input string to uppercase.
+
+public class makeUpperCase {
+    public static String makeUpperCaseMethod(String str){
+        return str.toUpperCase();
+    }
+    public static void main(String[] args) {
+        System.out.println(makeUpperCaseMethod("hello"));
+    }
+}
