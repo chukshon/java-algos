5cb2f4f964a4e7ce5c7d55750f0c9b38678bf33b
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-05 14:05:03 +0100

    declared a result variable

---
+++ mulitplicationTableForNumbers.java
@@ -0,0 +1,13 @@
+// Description: The goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+public class mulitplicationTableForNumbers {
+    public static String multiplicationTable(int number){
+        String result = "";
+      
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(multiplicationTable(5));
+    }
+    
+}
