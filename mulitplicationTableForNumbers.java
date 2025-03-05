4e3ead8d57431e4fb8df1740634d4b642f3a8372
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-05 14:05:54 +0100

    loop from 10 and added the multiplication string to the result variable

--- mulitplicationTableForNumbers.java
+++ mulitplicationTableForNumbers.java
@@ -3,7 +3,10 @@
 public class mulitplicationTableForNumbers {
     public static String multiplicationTable(int number){
         String result = "";
-      
+        
+          for(int i = 1; i <= 10; i++){
+            result += number + " x " + i + " = " + number * i + "\n";
+        }
         return result;
     }
     public static void main(String[] args) {
