af23a3614e9d4fc2329aa17d197e029561823fcd
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-05 13:09:41 +0100

    created a count character occurences method that takes in a string and character

---
+++ countingCharacters.java
@@ -0,0 +1,14 @@
+// Define a method named countCharOccurrences or count_char_occurrences that accepts a string and a char as inputs and 
+// returns the number of times the char occurs in the string as an int.
+
+
+public class countingCharacters {
+
+    public static int countCharOccurences(String string, char character){
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
