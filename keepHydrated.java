812446613db64b7d4c95759f3b9f84160972ae3d
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-20 14:09:48 +0100

    Take the time in hours and multiply by 0.5

---
+++ keepHydrated.java
@@ -0,0 +1,16 @@
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+
+
+public class keepHydrated{
+    public static int keepHydratedMethod(double time){
+       return time * 0.5;
+    }
+
+    public static void main(String[] args){
+        System.out.println(keepHydratedMethod(11.8));
+    }
+}
